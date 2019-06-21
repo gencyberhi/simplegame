780db8ab5d2b020a3980a753c8ddd15623269aba
kumueric <ericwen@hawaii.edu>
2019-06-21 11:02:44 -1000

    Better example.
    Uses modules.

--- simplegame.py
+++ simplegame.py
@@ -3,51 +3,65 @@ A simple game in Python.
 This program illustrates the use of several elements of Python.
 '''
 
-print('')
-print('Welcome to the simple game.')
+def init():
+    '''Initialize'''
+    print('')
+    print('Welcome to the simple game.')
+    
+    hunger = 100
+    inventory = {
+        "apples": 3
+        , "bananas": 2
+        , "rope": 1
+        , "maps": 0
+        , "keys": 1
+    }
+    print('')
+    return hunger, inventory
 
-hunger = 100
-inventory = {
-    "apples": 3
-    , "bananas": 2
-    , "rope": 1
-    , "maps": 0
-    , "keys": 1
-}
-print('')
+def show(hunger, inventory):
+    print('')
+    print('Hunger:', hunger)
+    print('INVENTORY')
+    for item, qty in inventory.items():
+        print('\t', item, ':', qty)
+    print('')
+
+def showhelp():
+    print('')
+    print('The following commands are recognized:')
+    print('    e item : Eat item')
+    print('    h      : Display this help.')
+    print('    s      : Show hunger & inventory.')
+    print('    q      : Quit this game.')
+    print('')
 
 
-text0 = ''
-while text0 != 'q' :
-    text = input('command > ')
-    text0 = text[0]
-    if text0 == 'h':
-        print('')
-        print('The following commands are recognized:')
-        print('    e item : Eat item')
-        print('    h      : Display this help.')
-        print('    s      : Show hunger & inventory.')
-        print('    q      : Quit this game.')
-        print('')
+def eat(item, inventory, hunger):
+    if item not in inventory.keys() or inventory[item] < 1:
+        print('OOPS, you do not have any', item, 'to eat!')
+        return
+    
+    inventory[item] -= 1
+    if item == 'apples':
+        hunger += 10
+    
+    print('You have', inventory[item], item, 'remaining and your hunger is: ', health)
 
-    elif text0 == 's':
-        print('')
-        print('Hunger:', hunger)
-        print('INVENTORY')
-        for item, qty in inventory.items():
-            print('\t', item, ':', qty)
-        print('')
 
-    elif text0 == 'e':
-        item = text[2:]
-        if item not in inventory.keys() or inventory[item] < 1:
-            print('OOPS, you do not have any', item, 'to eat!')
-        else:
-            inventory[item] -= 1
-            if item == 'apples':
-                hunger += 10
-            print('You have', inventory[item], item, 'remaining and your hunger is: ', health)
+if __name__ == '__main__':
+    hunger, inventory = init()
+    show(hunger, inventory)
 
-    hunger -= 2
-
+    text0 = ''
+    while text0 != 'q' :
+        text = input('command > ')
+        text0 = text[0]
+        if text0 == 'h':
+            showhelp()
+        elif text0 == 's':
+            show(hunger, inventory)
+        elif text0 == 'e':
+            eat(text[2:], inventory, hunger)
+        hunger -= 2
 
